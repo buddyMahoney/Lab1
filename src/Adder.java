60f23ff8b2c725114cef3416326f49c1c65d2ce7
buddyMahoney <wmahoney@carthage.edu>
2026-05-22 16:35:27 -0500

    Added getters and setters for latestSum.

--- src/Adder.java
+++ src/Adder.java
@@ -8,15 +8,26 @@ public class Adder {
 
     public double add(double num1, double num2)
     {
-        //double sum = latestSum;
-        latestSum = num1 + num2;
-        return latestSum;
+        double sum = num1 + num2;
+        setLatestSum(sum);
+        return sum;
     }
 
     public double add(double num1)
     {
-        //double sum = latestSum
-        latestSum = latestSum + num1;
+        double sum = getLatestSum();
+        sum = sum + num1;
+        setLatestSum(sum);
+        return sum;
+    }
+
+    private void setLatestSum(double ls)
+    {
+        latestSum = ls;
+    }
+
+    public double getLatestSum()
+    {
         return latestSum;
     }
 }
