db52c53a45a81e73e57c971c1d99bef8331a1219
buddyMahoney <wmahoney@carthage.edu>
2026-05-22 16:19:21 -0500

    Cleaned up prints.

--- src/Driver.java
+++ src/Driver.java
@@ -4,19 +4,19 @@ public class Driver {
         Adder adder = new Adder();
 
         double result = adder.add(2.5, 3.5);
-        System.out.println(result);
+        System.out.println("Answer: " + result);
         assert(result == 6.0);
 
         result = adder.add(10.0);
-        System.out.println(result);
+        System.out.println("Answer: " + result);
         assert(result == 16.0);
 
         result = adder.add(100.0, 100.0);
-        System.out.println(result);
+        System.out.println("Answer: " + result);
         assert(result == 200.0);
 
         result = adder.add(10.0);
-        System.out.println(result);
+        System.out.println("Answer: " + result);
         assert(result == 210.0);
     }
 }
